22e98218ecb0d2b2358301438f62d96f4ebbcfff
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-02-10 15:51:45 -0300

    iniciado desenvolvimento do frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+
+      <div>Sujeito Pizzaria :)</div>
+    </>
+  )
+}
