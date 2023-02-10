ae4d36f5a4b20809a857955ac7b28d64f71e3342
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-02-10 20:09:55 -0300

    realizado a tela de cadastro + alterações em login

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,17 +1,15 @@
-import { Html,Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document(){
-  return (
+  return(
     <Html>
       <Head>
 
       </Head>
-
       <body>
-        <Main />
-        <NextScript />
+        <Main/>
+        <NextScript/>
       </body>
-      
     </Html>
   )
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,39 +1,49 @@
-import Head from 'next/head';
+import Head from 'next/head'
 import Image from 'next/image';
-import styles from '../styles/Home.module.scss';
+import styles from '../../styles/home.module.scss';
+
 import logoImg from '../../public/logo.svg';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
+
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>SujeitoPizza - Faça seu login</title>
-      </Head>
+    <Head>
+      <title>SujeitoPizza - Faça seu login</title> 
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
-      <div className={styles.containerCenter}>
-        <Image src={logoImg} alt='logo PIzzaria'/>
-      
-        <div className={styles.login}>
-          <form>
-            <Input 
-              type='text'
-              placeholder='Digite seu E-mail' />
-            <Input 
-              type='password'
-              placeholder='Digite sua Senha' />
-            <Button
-              type='submit'
-              loading={false}>
-                Acessar
-            </Button>
-            
-          </form>
-          <a className={styles.text}>Não possui conta?Cadastre-se!</a>
-        </div>
+      <div className={styles.login}>
+        <form>
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+
+          <Input
+            placeholder="Digite sua Senha"
+            type="password"
+          />
+          
+          <Button
+            type="submit"
+            loading={false}
+          >
+            Acessar
+          </Button>
+        </form>
+
+        <Link href="/signup">
+           <a className={styles.text}>Nao possui uma conta? Cadastre-se</a>
+        </Link>
 
       </div>
+    </div>
     </>
   )
 }
