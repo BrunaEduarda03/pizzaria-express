d77f7210fa737d98adc0d22293dc17c7d1689f55
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-02-14 02:07:58 -0300

    finalizado tela de  produtos

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,7 +1,8 @@
 
 import Head from "next/head";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState,FormEvent } from "react";
 import { FiUpload } from "react-icons/fi";
+import { toast } from "react-toastify";
 import { Header } from "../../components/Header";
 import { setupAPIClient } from "../../services/api";
 import { canSSRAuth } from "../utils/canSSRAuh";
@@ -23,6 +24,9 @@ export default function Product({categoryList}:Categoryrops){
   const [avatarURL,setAvatarUrl] = useState('');
   const [imageAvatar,setImageAvatar] = useState('');
   const [categorySelected,setCategorySelected] = useState(0);
+  const [name,setName] = useState('');
+  const [price,setPrice] = useState(0);
+  const [description,setDescription] = useState('');
   
   async function handleFile(e:ChangeEvent<HTMLInputElement>){
     // console.log(e.target.files);   
@@ -44,6 +48,33 @@ export default function Product({categoryList}:Categoryrops){
     setCategorySelected(e.target.value);
   }
 
+  async function handleSubmit(e:FormEvent){
+    e.preventDefault();
+    try{
+      const data = new FormData();
+      if(name ==='' || price === null || description ==='' || imageAvatar ===''){
+        toast.error('Preencha todos os campos');
+        return;
+      }
+      data.append('name',name);
+      data.append('price',price.toLocaleString());
+      data.append('description',description);
+      data.append('category_id',categories[categorySelected].id);
+      data.append('file',imageAvatar);
+
+      const apiClient = setupAPIClient();
+      await apiClient.post('/product',data);
+      toast.success('produto cadastrado com sucesso')
+      setName('');
+      setDescription('');
+      setPrice(null);
+      setImageAvatar('');
+    }catch(error){
+      console.log(error);
+      toast('ops,erro ao cadastrar')
+    }
+  }
+
   return (
     <>
       <Head>
@@ -52,7 +83,8 @@ export default function Product({categoryList}:Categoryrops){
       <Header />
       <main className={styles.container}>
         <h1>Novo produto</h1>
-        <form className={styles.form}>
+
+        <form className={styles.form} onSubmit={handleSubmit}>
           <label className={styles.image}>
             <span>
               <FiUpload color="#FFF" size={25} />
@@ -94,15 +126,21 @@ export default function Product({categoryList}:Categoryrops){
             type='text'
             placeholder="Insira o nome do item"
             className={styles.input}
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
           
           />
           <input
             type='text'
-            placeholder="Insira o valor"  
+            placeholder="Preço do produto"  
             className={styles.input}
+            value={price}
+            onChange={(e)=>setPrice(e.target.value)}
           />
           <textarea 
             placeholder="Insira a descrição"
+            value={description}
+            onChange={(e)=>setDescription(e.target.value)}
           />
 
           
